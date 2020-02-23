19eada7a0548db8066a90bfd98087cb8b616c2cd
Liu <lc-soft@live.cn>
2020-02-23 19:48:03 +0800

    feat: update about page

--- src/ui/components/about.c
+++ src/ui/components/about.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <LCUI.h>
 #include <LCDesign.h>
+#include <lcui-router.h>
 #include <LCUI/gui/widget/textview.h>
 #include "version.h"
 #include "about.h"
@@ -14,6 +15,8 @@ void About_OnInit(LCUI_Widget w)
 	LCUI_Widget version;
 	LCUI_Widget os_info;
 	LCUI_Widget lcui_version;
+	LCUI_Widget lcui_router_version;
+	LCUI_Widget lc_design_version;
 	LCUI_Widget description;
 	LCUI_Widget time;
 	LCUI_Widget meta;
@@ -22,7 +25,9 @@ void About_OnInit(LCUI_Widget w)
 	LCUI_Widget item;
 
 	char os_info_str[32];
-	char lcui_version_str[32];
+	char lcui_version_str[64];
+	char lcui_router_version_str[64];
+	char lc_design_version_str[64];
 	char version_str[32];
 	char time_str[64];
 
@@ -41,23 +46,31 @@ void About_OnInit(LCUI_Widget w)
 	version = LCUIWidget_New("textview");
 	os_info = LCUIWidget_New("textview");
 	lcui_version = LCUIWidget_New("textview");
+	lcui_router_version = LCUIWidget_New("textview");
+	lc_design_version = LCUIWidget_New("textview");
 	description = LCUIWidget_New("textview");
 	meta = LCUIWidget_New(NULL);
 	item = LCUIWidget_New(NULL);
 	sprintf(version_str, "Version: %s", APP_VERSION);
 	sprintf(time_str, "Build at: %s", APP_BUILD_TIME);
 	sprintf(lcui_version_str, "LCUI: %s", LCUI_GetVersion());
+	sprintf(lcui_router_version_str, "LCUI Router: %s", router_get_version());
+	sprintf(lc_design_version_str, "LC Design: %s", LCDesign_GetVersion());
 	sprintf(os_info_str, "OS: %s", OS_INFO);
 	TextView_SetTextW(description, APP_DESCRIPTION);
 	TextView_SetText(version, version_str);
 	TextView_SetText(time, time_str);
 	TextView_SetText(lcui_version, lcui_version_str);
+	TextView_SetText(lcui_router_version, lcui_router_version_str);
+	TextView_SetText(lc_design_version, lc_design_version_str);
 	TextView_SetText(os_info, os_info_str);
 	Widget_AddClass(item, "c-about__item");
 	Widget_AddClass(meta, "c-about__meta");
 	Widget_Append(meta, version);
 	Widget_Append(meta, time);
 	Widget_Append(meta, lcui_version);
+	Widget_Append(meta, lcui_router_version);
+	Widget_Append(meta, lc_design_version);
 	Widget_Append(meta, os_info);
 	Widget_Append(item, description);
 	Widget_Append(item, meta);
